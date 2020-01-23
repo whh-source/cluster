24a4b14ff34bdc0e51e7142ef871f29874469b78
wanghuaihong1@konka.com <395055557@qq.com>
2020-01-23 09:14:52 +0800

    重写RedisTemplate

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # cluster(集群方式)
 
-如果想定主读从，要重写 RedisTemplate,
-可参考： https://github.com/whh-redis/redis-sentinel
+#### 1, redis 集群连接方式
+ 
+    如果想定主读从，要重写 RedisTemplate,
+    可参考： https://github.com/whh-redis/redis-sentinel
+
+#### 2, redis 能力发掘
+
+##### a) key - value 结构
+    redis 基本最常用的的是 key -value结构， 过于复杂的数据，可先转json 再存。
+    json的方式就是不灵活, 应该使用 key 
+        
--- src/test/java/com/whh/rediscluster/RedisClusterApplicationTests.java
+++ src/test/java/com/whh/rediscluster/RedisClusterApplicationTests.java
@@ -1,13 +1,16 @@
 package com.whh.rediscluster;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.whh.rediscluster.service.impl.RedisServiceImpl;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -15,29 +18,74 @@ import com.whh.rediscluster.service.impl.RedisServiceImpl;
 class RedisClusterApplicationTests {
 
 	@Autowired
-	private RedisServiceImpl redisService;
+	private RedisTemplate<String, Object> redisTemplate;
+
+	@Test
+	public void setTest(){
+
+	}
+
+	@Test
+	public void hashTest(){
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("name", "wanghuaihong");
+		map.put("sex", 1);
+		redisTemplate.opsForHash().putAll("worker", map);
+		redisTemplate.opsForHash().put("worker", "old", 18);
+
+		String name = (String) redisTemplate.opsForHash().get("worker", "name");
+		Integer sex = (Integer) redisTemplate.opsForHash().get("worker", "sex");
+
+		Integer old = (Integer) redisTemplate.opsForHash().get("worker", "old");
+
+		log.info("worker: {}, sex:{}, old: {}", name, sex, old);
+
+	}
+
+	@Test
+	public void listTest(){
+		redisTemplate.opsForList().rightPush("list1", "212");
+		String key = (String) redisTemplate.opsForList().leftPop("list1");
+		log.info("key:{}", key);
+
+		//等待3秒返回
+		String value = (String)redisTemplate.opsForList().leftPop("list1", 3, TimeUnit.SECONDS);
+		log.info("key:{}", value);
+
+
+		//立刻返回
+		value = (String)redisTemplate.opsForList().leftPop("list1");
+		log.info("key:{}", value);
+
+
+	}
 
 	@Test
 	public void stringTest(){
-		boolean ret = redisService.set("test", "this string");
-		Assert.assertTrue(ret);
+		ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+		if (valueOperations != null) {
+			Boolean success = valueOperations.setIfAbsent("asdf", "3425", 8, TimeUnit.HOURS);
+			if (success != null && success){
+				log.info("success");
+			}else {
+				log.info("fail");
+			}
 
-		String value = redisService.get("test");
-		Assert.assertEquals(value, "this string");
+			redisTemplate.opsForValue().set("asdf3", 13);
+			redisTemplate.opsForValue().increment("asdf3");
 
-		ret = redisService.set("test", "asdf");
-		Assert.assertTrue(ret);
+			Integer value = (Integer) valueOperations.get("asdf3");
+			log.info("value: {}", value);
 
-		value = redisService.get("test");
-		Assert.assertEquals(value, "asdf");
+			redisTemplate.opsForValue().set("jj3", "asdfasdf");
 
-		ret = redisService.set("test", "dddd");
-		Assert.assertTrue(ret);
+			String tmp = (String) redisTemplate.opsForValue().get("jj3");
+			log.info("value: {}", tmp);
 
-		value = redisService.get("test");
-		Assert.assertEquals(value, "dddd");
-
-		log.info("test value:{}", value);
+			String oldValue = (String) redisTemplate.opsForValue().getAndSet("jj3", "sd");
+			log.info("value: {}", oldValue);
+		}
 	}
+
 
 }
